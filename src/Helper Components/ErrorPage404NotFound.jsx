14330d275adf914c3ddf3300d63155c9faf30bba
Sharan Kudtarkar <SK19@ador.co.in>
2023-11-21 12:26:38 +0530

    change in error page

--- src/Helper Components/ErrorPage404NotFound.jsx
+++ src/Helper Components/ErrorPage404NotFound.jsx
@@ -13,19 +13,21 @@ export function NotFoundTitle() {
         window.location.href = "/login"
     }
     return (
-        <div className="text-center grid gap-3">
-            <span className="w-[100%]  h-[100%] text-[40rem] top-0  absolute  text-[#f8f8f8]">404</span>
-            <span className="text-center text-[3rem] font-semibold font-sans text-[#585858]">You have found a secret place.</span>
-            <span className="text-center font-sans">
-                Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has
-                been moved to another URL.
-            </span>
-            <div >
-                <Button style={{
-                    backgroundColor: "#acacac",
-                }} variant="contained" onClick={() => take_me_back_to_home_page()}>
-                    Take me back to login page
-                </Button>
+        <div className="text-center grid ">
+            <span className="text-[20rem] text-[#f8f8f8]">404</span>
+            <div className='grid gap-3 mt-[-4rem]'>
+                <span className="text-center text-[3rem] font-semibold font-sans text-[#585858]">You have found a secret place.</span>
+                <span className="text-center font-sans">
+                    Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has
+                    been moved to another URL.
+                </span>
+                <div >
+                    <Button style={{
+                        backgroundColor: "#acacac",
+                    }} variant="contained" onClick={() => take_me_back_to_home_page()}>
+                        Take me back to login page
+                    </Button>
+                </div>
             </div>
         </div>
     );
