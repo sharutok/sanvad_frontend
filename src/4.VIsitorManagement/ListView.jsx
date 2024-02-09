1967fe73b7dd2872ac8ec3ba100228110baa9696
Sharan Kudtarkar <SK19@ador.co.in>
2024-02-09 11:41:52 +0530

    02/09/24 11:41:44

--- src/4.VIsitorManagement/ListView.jsx
+++ src/4.VIsitorManagement/ListView.jsx
@@ -48,8 +48,8 @@ export default function VisitManagementListView() {
     })
 
     function handleNav(g) {
-        // window.location.href = "/vistors/management/" + g.id
-        window.open("/vistors/management/" + g.id, '_blank')
+        window.location.href = "/vistors/management/" + g.id
+        // window.open("/vistors/management/" + g.id, '_blank')
     }
 
     async function handleDelete(id) {
